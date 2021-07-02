8874343e061f1056080391c02ac1cc1e58890d72
Manoele Ribeiro <86082607+ManuRss@users.noreply.github.com>
2021-07-02 17:12:32 -0300

    dados_clientes

--- cliente_dados.java
+++ cliente_dados.java
@@ -1,3 +1,31 @@
-public interface cliente_dados {
+public class cliente_dados {
+
+    private String nome;
+    private String cpf;
+    private String job;
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getJob(){
+        return job;
+    }
+
+    public void setJob(String job){
+        this.job = job;
+    }
+
     
 }
