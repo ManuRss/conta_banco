159f1d37152057e43556b56fa7731e07900f28a7
Manoele Ribeiro <86082607+ManuRss@users.noreply.github.com>
2021-07-02 17:12:45 -0300

    conta_mod

--- conta.java
+++ conta.java
@@ -1,40 +1,24 @@
 public class conta {
 
-    private static int total;
-    double saldo;
+    public double saldo;
     private int agencia;
-    private int numero;
-    private String titular;
-    double limite; deposito;
-    double valor, transferir, sacar; 
-    String conta_destino;
+    private int numero_conta; 
+    private cliente_dados titular;
+    double limite;
+   
+    private static int contatotal;
 
-    public conta(int agencia, int numero){
-        conta.total++;
-        System.out.println("O total de contas é " + conta.total);
-        this.setAgencia(agencia);
-        this.numero = numero;
-        this.saldo = 100; 
-        System.out.println("Estou criando uma conta" + this.numero);
-    }
+    //MÉTODOS
 
-    //métodos
-
-    public String getTitular() {
-        return titular;
-    }
-
-    public void setTitular(String titular) {
-        this.titular = titular;
-    }
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public void setAgencia(int agencia) {
+    public conta(int agencia, int numero_conta){
         this.agencia = agencia;
+        this.numero_conta = numero_conta;
+        contatotal++;
+        System.out.println("A conta " + this.numero_conta + "foi criada com êxito!");
+        
     }
+
+    //métodos operações na conta
 
     public void deposito(double valor){
         this.saldo+=valor;
@@ -48,9 +32,11 @@ public class conta {
         return false;
     }
 
+// usa-se this.sacar pois a transferencia é entendida como um saque na conta
+
     public boolean transferir(double valor, conta conta_destino){
         if(this.saldo >= valor){
-            this.transferir(valor, conta_destino);
+            this.sacar(valor);
             conta_destino.deposito(valor);
             return true;
         }
@@ -61,8 +47,33 @@ public class conta {
         return saldo;
     }
 
-    public double setSaldo(double saldo){
-        return this.saldo;
+    public double getLimite(){
+        return limite;
+    }
+
+    public void setLimite(double limite){
+        this.limite = limite;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero_conta() {
+        return numero_conta;
+    }
+
+    public cliente_dados getTitular() {
+        return titular;
+    }
+
+    public void setTitular(cliente_dados titular) {
+        this.titular = titular;
+    }
+
+  
+    public static int getContatotal() {
+        return contatotal;
     }
 
     public void Sair(){
