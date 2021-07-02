53c817ef74da06d1726ed615758413be120359b8
Manoele Ribeiro <86082607+ManuRss@users.noreply.github.com>
2021-07-02 20:41:32 -0300

    Create teste_banco.java

---
+++ teste_banco.java
@@ -0,0 +1,26 @@
+public class teste_banco{
+    public void main(String[] args){
+        //CRIAR CONTA NO BANCO
+        conta suaConta = new conta(0152, 896541);
+        conta segConta = new conta(0152, 987412);
+
+        conta.setTitular(new Client());
+        conta.getTitular().setNome("Laila");
+        conta.getTitular().setCpf("0101552036");
+        conta.getTitular().setJob("Astronauta");
+
+        suaConta.deposito(500);
+        suaConta.sacar(50);
+        suaConta.transferir(250, segConta);
+        segConta.sacar(100);
+        segConta.deposito(20);
+
+
+
+
+
+
+
+
+    }
+}
